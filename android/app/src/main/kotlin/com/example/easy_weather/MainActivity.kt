8cea475b10c8975b41d221eceaa4e325ae612bef
spcodegen <shehansp2000@gmail.com>
2024-09-26 11:55:36 +0530

    intial Project

---
+++ android/app/src/main/kotlin/com/example/easy_weather/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.easy_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:easy_weather/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
